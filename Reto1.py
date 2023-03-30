22833188b430864902a983e2d85729c0ac103b62
morenoca16 <125147005+morenoca16@users.noreply.github.com>
2023-03-29 22:08:58 -0500

    Update Reto1.py

--- Reto1.py
+++ Reto1.py
@@ -1,46 +1,41 @@
-#1.Crear clase
 class Persona:
-    tipoDoc=" "
-    documento=0
-    nombre=" "
-    apellido=" "
-    peso=0
-    estatura=0.5
-    edad=0
-    sexo=" "
-#7.Método constructor
-    def __init__(self, tipoDoc, documento, nombre, apellido, peso, estatura, edad, sexo):
-        self.tipoDoc=tipoDoc
-        self.documento=documento
-        self.nombre=nombre
-        self.apellido=apellido
-        self.peso=peso
-        self.estatura=estatura
-        self.edad=edad
-        self.sexo=sexo
-    def registrarPersona(self):
-        self.tipoDoc=input("Ingrese tipo de documento: ")
-        self.documento=int(input("Ingresar número de documento: "))
-        self.nombre=input("Ingrese el nombre de la persona: ")
-        self.apellido=input("Ingrese el apellido de la persona: ")
-        self.peso=int(input("Ingrese el peso: "))
-        self.estatura=int(input("Ingresar la estatura: "))
-        self.edad=int(input("Ingresar la edad: "))
-        self.sexo=input("Ingresar sexo: ")
+    def __init__(self):
+        self.tipoDoc = ""
+        self.documento = ""
+        self.nombre = ""
+        self.apellido = ""
+        self.peso = 0.0
+        self.estatura = 0.0
+        self.edad = 0
+        self.sexo = ""
 
-#7.Definir método con parámetros luego del contructor
-    def mostrarPersonas(self):
+    def pedirDatos(self):
+        self.tipoDoc = input("Ingrese Tipo de documento: ")
+        self.documento = input("Ingrese Número de documento: ")
+        self.nombre = input("Ingrese Nombre: ")
+        self.apellido = input("Ingrese Apellido: ")
+        self.peso = float(input("Ingrese Peso (kilogramosg): "))
+        self.estatura = float(input("Ingrese Estatura (metros): "))
+        self.edad = int(input("Ingrese edad: "))
+        self.sexo = input("Ingrese Sexo: ")
+
+    def mostrarDatos(self):
         print(f'Tipo de documento: {self.tipoDoc} \nNumero de documento: {self.documento} \nNombre: {self.nombre}\nApellido: {self.apellido}\nPeso: {self.peso}\nEstatura: {self.estatura}\nEdad: {self.edad}\nSexo: {self.sexo}')
-      
-    def calcularIMC(self):
-      pesoActual=self.peso/(self.estatura**2)
-      print(f'el ususario {self.nombre} {self.apellido} \nidentificado con el tipo de documento: {self.tipoDoc} \n tiene un peso de {pesoActual}')
-      return pesoActual
-    def CalcularMayorEdad(self):
-      if self.edad>=18:
-        print('El usuario ya es mayor de edad')
-      else:
-        print('El usuario aun no es mayor de edad')
+
+    def calcularImc(self):
+        pesoActual = self.peso / (self.estatura ** 2)
+        if pesoActual < 20:
+            print("El peso está por debajo de lo ideal")
+        elif pesoActual >= 20 and pesoActual <= 25:
+            print("El peso es ideal")
+        else:
+            print("Tiene sobrepeso")
+
+    def mayorEdad(self):
+        if self.edad >= 18:
+            print("Mayor de edad")
+        else:
+            print("Menor de edad")
 
 '''class Persona:
     def __init__(self, nombre, edad):
