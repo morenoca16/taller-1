edeae2cd90a987fca0a908ff04470921df136dec
morenoca16 <125147005+morenoca16@users.noreply.github.com>
2023-03-29 22:48:31 -0500

    Encapsular
    Encapsular

---
+++ Reto3.py
@@ -0,0 +1,86 @@
+class Persona:
+    def __init__(self):
+        self.tipoDoc = ""
+        self.documento = ""
+        self.nombre = ""
+        self.apellido = ""
+        self.peso = 0.0
+        self.estatura = 0.0
+        self.edad = 0
+        self.sexo = ""
+
+    def pedirDatos(self):
+        self.tipoDoc = input("Ingrese Tipo de documento: ")
+        self.documento = input("Ingrese Número de documento: ")
+        self.nombre = input("Ingrese Nombre: ")
+        self.apellido = input("Ingrese Apellido: ")
+        self.peso = float(input("Ingrese Peso (kilogramosg): "))
+        self.estatura = float(input("Ingrese Estatura (metros): "))
+        self.edad = int(input("Ingrese edad: "))
+        self.sexo = input("Ingrese Sexo: ")
+
+    def mostrarDatos(self):
+        print(f'Tipo de documento: {self.tipoDoc} \nNumero de documento: {self.documento} \nNombre: {self.nombre}\nApellido: {self.apellido}\nPeso: {self.peso}\nEstatura: {self.estatura}\nEdad: {self.edad}\nSexo: {self.sexo}')
+
+    def calcularImc(self):
+        pesoActual = self.peso / (self.estatura ** 2)
+        if pesoActual < 20:
+            print("El peso está por debajo de lo ideal")
+        elif pesoActual >= 20 and pesoActual <= 25:
+            print("El peso es ideal")
+        else:
+            print("Tiene sobrepeso")
+
+    def mayorEdad(self):
+        if self.edad >= 18:
+            print("Mayor de edad")
+        else:
+            print("Menor de edad")
+
+##Encontre 2 formas 
+
+## Primera
+    __atributo_privado = "Soy un atributo inalcanzable desde fuera."
+
+e = Persona()
+print(e.__atributo_privado)
+
+def __pedirDatos(self):
+    print("Soy un método inalcanzable desde fuera.")
+
+e = Persona()
+e._pedirDatos()
+    
+__atributo_privado = "Soy un atributo inalcanzable desde fuera."
+
+def __metodo_privado(self):
+    print("Soy un método inalcanzable desde fuera.")
+
+def atributo_publico(self):
+    return self.__atributo_privado
+
+def metodo_publico(self):
+        return self.__metodo_privado()
+
+e = Persona()
+print(e.atributo_publico())
+e.metodo_publico()
+
+##Segunda
+
+'''class Persona:
+    def __init__(self, nombre, edad):
+        self._nombre = nombre  # atributo privado
+        self._edad = edad  # atributo privado
+    def get_nombre(self):
+        
+       
+return self._nombre  # método público
+    def set_nombre(self, nombre):
+        
+       
+self._nombre = nombre  # método público
+    def get_edad(self):
+        return self._edad  # método público
+    def set_edad(self, edad):
+        self._edad = edad  # método público'''
